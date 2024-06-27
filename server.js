bede159e92adc4e5793cd6622507992acf422022
Emyli Quadros <babaloogames@gmail.com>
2024-06-27 10:39:12 -0300

    crud tipo sala

---
+++ server.js
@@ -0,0 +1,16 @@
+
+import express from "express";
+import routes from "./startup/routes.js";
+const db = require('./startup/db.js');
+import cors from "cors"; 
+
+const app = express();
+
+app.use(cors()); 
+
+app.use(express.json());
+app.use(routes);
+
+db.sync(() => console.log(`Banco de dados conectado: ${process.env.DB_NAME}`));
+
+app.listen(3000, () => console.log("Servidor iniciado na porta 3000"));
