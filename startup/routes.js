8ea26e01d09432295c3d2ad482e32244084247e5
Emyli Quadros <babaloogames@gmail.com>
2024-07-01 10:09:27 -0300

    add route to SalaController

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import TipoSalaController from "../controller/TipoSalaController.js"
+import SalaController from "../controller/SalaController.js";
 
 const routes = express.Router();
 
 // TipoSala
 
 routes.post("/api/tipoSala/", TipoSalaController.create)
+
+// Sala
+
+routes.post("/api/sala/", SalaController.create)
 
 export { routes as default };
