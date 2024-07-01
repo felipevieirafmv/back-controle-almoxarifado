3aa7fffe47a7cb99b717abb64c102f74114d159b
Emyli Quadros <babaloogames@gmail.com>
2024-07-01 10:16:39 -0300

    add route to TipoFuncionarioController

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import TipoSalaController from "../controller/TipoSalaController.js"
 import SalaController from "../controller/SalaController.js";
+import TipoFuncionarioController from "../controller/TipoFuncionarioController.js";
 
 const routes = express.Router();
 
@@ -11,5 +12,10 @@ routes.post("/api/tipoSala/", TipoSalaController.create)
 // Sala
 
 routes.post("/api/sala/", SalaController.create)
+
+// TipoFuncionario
+
+routes.post("/api/tipoFuncionario", TipoFuncionarioController.create)
+
 
 export { routes as default };
